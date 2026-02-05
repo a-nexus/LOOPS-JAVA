188692d01312e990360216a4051df3465171e1df
a-nexus <anuragsingh77077@gmail.com>
2026-02-05 23:40:22 +0530

    EXTRA QUESTIONS

--- Pattern.java
+++ Pattern.java
@@ -82,3 +82,63 @@
 //     }
 // }
 
+//NEXT QUESTION PRINT HALF PYRAMID ROTATED 180 DEGREEE 
+
+
+// import java.util.*;
+// public class Pattern{
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         System.out.print("ENTER HOW MANY ROW YOU WANT:");
+//         int row=sc.nextInt();
+//         for(int i=1;i<=row;i++){
+//             for(int j=1;j<=row-i;j++){
+//                 System.out.print(" ");
+//             }for(int j=1;j<=i;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+
+//     }
+// }
+
+//NEXT QUESTION
+//PRINT THE PATTERN HALF PYRAMID WITH NUMBER 
+
+// import java.util.*;
+// public class Pattern{
+//     public static void main(String [] args){
+//          Scanner sc=new Scanner(System.in);
+//          System.out.print("ENTER HOW MANY ROWS YOU WANT:");
+//          int row=sc.nextInt();
+//          for(int i=1;i<=row;i++){
+//             for(int j=1;j<=i;j++){
+//                 System.out.print(j);
+//             }System.out.println();
+//          }
+//     }
+// }
+
+
+//NEXT QUESTION
+//PRINT THE PATTERN INVERTED HALF PYRAMID WITH NUMBER
+
+import java.util.*;
+
+
+
+
+public class Pattern{
+    public static void main(String[] args){
+        Scanner sc=new Scanner (System.in);
+        System.out.print("ENTER HOW MANY ROW YOU WANT:");
+        int row=sc.nextInt();
+        for(int i=row;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
