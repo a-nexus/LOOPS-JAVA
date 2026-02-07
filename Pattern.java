9bc8c6a11c288b869f9fac45bfb6fdf4124d5e71
a-nexus <anuragsingh77077@gmail.com>
2026-02-07 20:26:39 +0530

    EXTRA QUESTION

--- Pattern.java
+++ Pattern.java
@@ -166,21 +166,50 @@
 //NEXT QUESTION 
 //PRINT THE PATTERN 0-1 TRIANGLE
 
-import java.util.*;
-public class Pattern{
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        System.out.print("ENTER HOW MANY ROWS YOU WANT:");
-        int num=sc.nextInt();
-           for(int i=1;i<=num;i++){
-            for(int j=1;j<=i;j++){
-                int sum=i+j;
-                if(sum%2==0){
-                    System.out.print("1 ");
-                }else{
-                    System.out.print("0 ");
-                }
-            }System.out.println();
-           }
-    }
-}
+// import java.util.*;
+// public class Pattern{
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         System.out.print("ENTER HOW MANY ROWS YOU WANT:");
+//         int num=sc.nextInt();
+//            for(int i=1;i<=num;i++){
+//             for(int j=1;j<=i;j++){
+//                 int sum=i+j;
+//                 if(sum%2==0){
+//                     System.out.print("1 ");
+//                 }else{
+//                     System.out.print("0 ");
+//                 }
+//             }System.out.println();
+//            }
+//     }
+// }
+
+
+//EXTRA QUESTION
+//PRINT A SOLID RHOMBUS.
+
+// import java.util.*;
+// public class Pattern{
+//     public static void main(String[] args){
+//      Scanner sc=new Scanner(System.in);
+//      System.out.print("ENTER HOW MANY ROWS YOU WANT:");
+//      int row=sc.nextInt();
+//      for(int i=1;i<=row;i++){
+//         for(int j=row-1;j>=i;j--){
+//             System.out.print(" ");
+//         }for(int j=1;j<=row;j++){
+//             System.out.print("* ");
+//         }System.out.println();
+//      }
+//     }
+// }
+
+
+
+   
+
+
+    
+
+
