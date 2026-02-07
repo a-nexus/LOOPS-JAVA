ed37ec968dfe7bb5a8ce09d7f3ce4087fdf8ed73
a-nexus <anuragsingh77077@gmail.com>
2026-02-07 20:37:45 +0530

    BUTTERFLY PATTERN

--- Pattern.java
+++ Pattern.java
@@ -206,7 +206,41 @@
 // }
 
 
+//NEXT QUESTION
+//PRINT THE BUTTERFLY PATTERN
 
+import java.util.*;
+public class Pattern{
+public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.print("ENTER THE NUMBER OF ROWS:");
+    int row=sc.nextInt();
+    for(int i=1;i<=row;i++){
+        for(int j=1;j<=i;j++) {
+            System.out.print("*");
+        }for(int j=row-1;j>=i;j--){
+            System.out.print(" ");
+        }for(int j=row-1;j>=i;j--){
+            System.out.print(" ");
+        }
+        for(int j=1;j<=i;j++){
+             System.out.print("*");
+        } System.out.println();
+     } for(int i=1;i<=row;i++){
+        for(int j=i;j<=row;j++){
+            System.out.print("*");
+        }for(int j=row;j<row+i-1;j++){
+            System.out.print(" ");
+    }
+    for(int j=row;j<row+i-1;j++){
+        System.out.print(" ");
+    }for(int j=i;j<=row;j++){
+        System.out.print("*");
+    }System.out.println();
+         
+        }
+    }
+}
    
 
 
