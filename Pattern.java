c5eadc2a3b77b57a4825be39c139737d3c860c35
a-nexus <anuragsingh77077@gmail.com>
2026-02-08 22:43:16 +0530

    PATTERN DIAMOND

--- Pattern.java
+++ Pattern.java
@@ -306,5 +306,34 @@
 //     }
 // }
 
+//NEXT QUESTION
+//PRINT THE PATTERN DIAMOND
+
+import java.util.*;
+public class Pattern{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("ENTER THE NUMBER OF ROWS:");
+        int row=sc.nextInt();
+        for(int i=1;i<=row;i++){
+            for(int j=row-1;j>=i;j--){
+                System.out.print(" ");
+            }for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }for(int j=1;j<i;j++){
+                System.out.print("*");
+            }System.out.println();
+        }
+        for(int i=1;i<=row;i++){
+            for(int j=1;j<i;j++){
+                System.out.print(" ");
+            }for(int j=row;j>=i;j--){
+                System.out.print("*");
+            }for(int j=row-1;j>=i;j--){
+                System.out.print("*");
+            }System.out.println();
+        }
+    }
+}
 
 
